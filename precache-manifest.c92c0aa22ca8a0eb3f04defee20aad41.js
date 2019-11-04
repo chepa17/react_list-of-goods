69f064b2f94cfeffa8e813787f455ef67f47b48e
Chepkyi Mykola <chepkiyn@gmail.com>
2019-11-04 10:18:18 +0200

    Updates

---
+++ precache-manifest.c92c0aa22ca8a0eb3f04defee20aad41.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abe0d48dadf274979e09ce950d993f9f",
+    "url": "/react_list-of-goods/index.html"
+  },
+  {
+    "revision": "e708bed1a49773a4f0c3",
+    "url": "/react_list-of-goods/static/js/2.e17ad3c2.chunk.js"
+  },
+  {
+    "revision": "6aad2ac377dbad00753d",
+    "url": "/react_list-of-goods/static/js/main.f3d541fc.chunk.js"
+  },
+  {
+    "revision": "e82fdff4b55e53116e45",
+    "url": "/react_list-of-goods/static/js/runtime~main.14fd7ef8.js"
+  }
+]);
